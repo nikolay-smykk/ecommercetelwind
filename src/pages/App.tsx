65e83493e0eb4574c6b991ab32a3c17c3a0f2ff1
Nikolay Smyk <nikolaysmyk3@gmail.com>
2021-12-25 15:04:14 +0300

    add router

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { About } from './About';
+import { Home } from './Home';
 
 function App() {
   return (
-    <div className="App">
-      some
-    </div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+  </Routes>
   );
 }
 
